92a23013d80f206082667e52b5a27fded979e6c3
KelvinWu31 <60585581+KelvinWu31@users.noreply.github.com>
2021-08-05 19:40:56 -0400

    removed spaces in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <Paper style={{padding: "5px", display: "flex"}} square elevation={3}>
             <img className="logo-header" src="https://img.icons8.com/officel/2x/react.png"></img>
-            <h1 className="site-header"> New Contact ListJS</h1>
+            <h1 className="site-header"> NewContactListJS</h1>
         </Paper>
     )
 }
